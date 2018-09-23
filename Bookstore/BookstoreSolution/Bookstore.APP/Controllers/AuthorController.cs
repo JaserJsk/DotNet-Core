d2e14de2605fef5012cb0178070389095963baa8
Jonas Jsk <JonasJsk@outlook.com>
2018-09-23 18:19:47 +0200

    Removed un-needed files

--- Bookstore/BookstoreSolution/Bookstore.APP/Controllers/AuthorController.cs
+++ Bookstore/BookstoreSolution/Bookstore.APP/Controllers/AuthorController.cs
@@ -1,45 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using Bookstore.API.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bookstore.APP.Controllers
 {
-    public class AuthorsController : Controller
+    public class AuthorController : Controller
     {
-        // GET: Authors
+        // GET: Author
         public ActionResult Index()
         {
-            //1. Declare Client
-            HttpClient client = new HttpClient();
-            //2.Get URL
-            client.BaseAddress = new Uri("http://localhost:44315/");
-            //3.Declare Xml Or Json
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            //4. Get Data
-            HttpResponseMessage response = client.GetAsync("api/authors").Result;
-            var author = response.Content.ReadAsAsync<IEnumerable<Author>>().Result;
-            return View(author);
+            return View();
         }
 
-        // GET: Authors/Details/5
+        // GET: Author/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: Authors/Create
+        // GET: Author/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Authors/Create
+        // POST: Author/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(IFormCollection collection)
@@ -56,13 +44,13 @@ namespace Bookstore.APP.Controllers
             }
         }
 
-        // GET: Authors/Edit/5
+        // GET: Author/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: Authors/Edit/5
+        // POST: Author/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, IFormCollection collection)
@@ -79,13 +67,13 @@ namespace Bookstore.APP.Controllers
             }
         }
 
-        // GET: Authors/Delete/5
+        // GET: Author/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: Authors/Delete/5
+        // POST: Author/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
